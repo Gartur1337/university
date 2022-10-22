54c99ad97546d9baccaedd5161684a73c915796a
Gartur1337 <gakiev13@mail.ru>
2022-10-23 02:26:09 +0300

    Lab1(2)

--- lab2.cpp
+++ lab2.cpp
@@ -8,45 +8,54 @@
 using namespace std;
 
 
-double gen_numbers() {
+double genNumbers() {
     random_device rd;
     mt19937 gen(rd());
     uniform_int_distribution<int> distribution(-30,100);
     return distribution(gen);
 }
 
-
-// vector <int> &create_vector(vector <int> &vec){
-//     const int size = 100;
-//     int max_elem = 100;
-//     vec.reserve(size);
-//     for (int i = 0; i < size; i++){
-//         vec.push_back(gen_numbers());
-//     }
-//     return vec;
-// }
-
-void calc(){
-    vector <int> vec;
-    const int size = 10000;
+int calculation(vector <int> vec){
     int max_elem = 100;
-    vec.reserve(size);
-
-     for (int i = 0; i < size; i++){
-        vec.push_back(gen_numbers());
-    }
 
     for (auto number: vec){
         if (number < max_elem && number > 0){
             max_elem = number;
         }
     }
+    return max_elem;
+}
+
+void threadStarter(int thread_count){
+    vector <int> vec;
+    vector <thread> thread_vector;
+    thread_vector.reserve(thread_count);
+    const int size = 10000000;
+    int max_elem = 100;
+    vec.reserve(size);
+
+    for (int i = 0; i < size; i++){
+        vec.push_back(genNumbers());
+    }
+
+    size_t sample_size = vec.size() / thread_count;
+    for (int i = 0; i < vec.size(); i += sample_size){
+        vector<int> int_vector(vec.begin() + i, vec.begin() + i + sample_size); 
+        thread_vector.push_back(thread(calculation, int_vector));
+    }
+
+    for (int i = 0; i < thread_count; i++){
+        thread_vector.at(i).join();
+    }
 
 }
 
-
 int main()
 {
-    calc();
+    
+    threadStarter(4);
+    // threadStarter(10);
+    // threadStarter(20);
+    // threadStarter(40);
     return 0;
 }
