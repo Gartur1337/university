5aad64bb833fea36da7dcf6190b69801050e5413
Gartur1337 <gakiev13@mail.ru>
2022-10-26 17:48:23 +0300

    confirmed labs

--- lab3.cpp
+++ lab3.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <algorithm>
 #include <iterator>
+#include <omp.h>
 
 using namespace std;
 
@@ -32,20 +33,22 @@ void threadStarter(int thread_count){
     vector <thread> thread_vector;
     thread_vector.reserve(thread_count);
     generate(vec.begin(), vec.end(), genNumbers);
-
-    size_t sample_size = vec.size() / thread_count;
-    for (int i = 0; i < vec.size(); i += sample_size){
-        vector<int> int_vector(vec.begin() + i, vec.begin() + i + sample_size); 
-        thread_vector.push_back(thread(calculation, int_vector));
-    }
-
-    for (int i = 0; i < thread_count; i++){
-        thread_vector.at(i).join();
-    }
+    calculation(vec);
+//    size_t sample_size = vec.size() / thread_count;
+//    for (int i = 0; i < vec.size(); i += sample_size){
+//        vector<int> int_vector(vec.begin() + i, vec.begin() + i + sample_size);
+//        thread_vector.push_back(thread(calculation, int_vector));
+//    }
+//
+//    for (int i = 0; i < thread_count; i++){
+//        thread_vector.at(i).join();
+//    }
 }
 
 int main()
 {
+    omp_set_num_threads(4);
+    #pragma omp parallel default(none)
     threadStarter(4);
     // threadStarter(10);
     // threadStarter(20);
